94080fc7a8f155e2889b27dae1493db7ec211ae5
Freumode <ashelenglish@gmail.com>
2025-09-09 17:21:49 +0500

    database initialization started

--- src/main/java/utils/HibernateUtil.java
+++ src/main/java/utils/HibernateUtil.java
@@ -1,19 +1,26 @@
 package utils;
 
+import exception.DataBaseException;
+import exception.NotFoundException;
 import lombok.Getter;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import model.Match;
 import model.Player;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 @Slf4j
 @UtilityClass
 public class HibernateUtil {
     @Getter
     private static final SessionFactory sessionFactory = buildSessionFactory();
-
     private static SessionFactory buildSessionFactory() {
         try {
             log.debug("Building Hibernate SessionFactory");
@@ -33,6 +40,19 @@ public class HibernateUtil {
     }
 
     public static void initDatabase() {
-
+        try (InputStream resource = HibernateUtil.class.getClassLoader().getResourceAsStream("someDataForPagination.sql");
+        Session session = sessionFactory.openSession()
+        ) {
+            byte[] bytes = Objects.requireNonNull(resource).readAllBytes();
+            String sql = new String(bytes);
+            session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException exception) {
+            throw new DataBaseException("Database error");
+        }
+        catch (IOException exception) {
+            throw new NotFoundException("sql data is not found");
+        }
     }
 }
